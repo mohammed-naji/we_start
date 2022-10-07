076523ced03efda51d3c8c952c50dc5bf322a5ef
Mohammed Naji <m.naji1512@gmail.com>
2022-10-07 19:40:49 +0300

    lec8

---
+++ lec8-JS/js/script.js
@@ -0,0 +1,101 @@
+// console.log('Message')
+// console.error('Error')
+// console.warn('Warning')
+
+// var x = 'Ali';
+// x = 'Ezz';
+
+// let age = 20;
+// age = 18;
+
+// const age = 20;
+// age = 15;
+
+// console.log(typeof 'name' == 'string');
+
+// let age = null;
+
+// console.log(typeof age);
+
+// let name = 'Mohammed';
+// let age = 28;
+// let text = 'Welcome '+name+', your age is ' + age
+
+// let p = "<div class='new-box'>";
+//     p += "<h3>"+name+"</h3>";
+//     p += "<p>"+age+"</p>";
+//     p += "<a>hghgh</a>";
+//     p += "</div>";
+
+// let p = `
+// <div class="new-box">
+//     <h3>${name}</h3>
+//     <p>${age}/p>
+//     <a href="">fffff</a>
+// </div>
+// `;
+
+// document.write('Hiiiiiii');
+
+// console.log(text)
+
+// console.log(document);
+
+// console.log(10 == 10);
+
+// if(10 == 12) {
+//     console.log('Done');
+// }else {
+//     console.log('Error');
+// }
+
+age = 'dddd';
+
+// console.log(isNaN(2));
+
+// if(age > 0) {
+//     if(age <= 18) {
+//         console.log('انت لسا بيبي');
+//     }else if (age > 18 && age < 60) {
+//         console.log('انت في اصعب مرحلة عمرية قلبي يعتصر الما');
+//     }else {
+//         console.log('انت اسطووووورة اصلي بس اعمل مشاكل في البيت وصحينا يوم الجمعة من ال 6');
+//     }
+// }else {
+//     console.log('خف الهبل شوية ي حبيبي');
+// }
+
+// let char = 1;
+
+// switch(char) {
+//     case 1 :
+//     case 2 :
+//         console.log('انت الان في قائمة المبيعات');
+//     break;
+//     // case 2 :
+//     //     console.log('انت الان في قائمة المشتريات');
+//     // break;
+//     case 3 :
+//         console.log('انت في قسم الدعم الفني');
+//     break;
+
+//     default:
+//         console.log('Error');
+// }
+
+let number = [1, 5 , 5, 8, 6, 10];
+// console.log(number[0]);
+// for( let i = 0 ; i < 6 ; i++ ) {
+//     console.log(number[i]);
+// }
+// let x = 5;
+// console.log(++x);
+
+// i++ 
+// ++i
+
+let i = 0;
+while(i < 6) {
+    console.log(number[i] + ' Whileeee')
+    i++;
+}
---
+++ lec8-JS/js/youtube.js
@@ -0,0 +1,29 @@
+function labnolIframe(div) {
+    var iframe = document.createElement('iframe');
+    iframe.setAttribute('src', 'https://www.youtube.com/embed/' + div.dataset.id + '?autoplay=1&rel=0');
+    iframe.setAttribute('frameborder', '0');
+    iframe.setAttribute('allowfullscreen', '1');
+    iframe.setAttribute('allow', 'accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture');
+    div.parentNode.replaceChild(iframe, div);
+  }
+
+  function initYouTubeVideos() {
+    var playerElements = document.getElementsByClassName('youtube-player');
+    for (var n = 0; n < playerElements.length; n++) {
+      var videoId = playerElements[n].dataset.id;
+      var div = document.createElement('div');
+      div.setAttribute('data-id', videoId);
+      var thumbNode = document.createElement('img');
+      thumbNode.src = 'https://i.ytimg.com/vi/ID/hqdefault.jpg'.replace('ID', videoId);
+      div.appendChild(thumbNode);
+      var playButton = document.createElement('div');
+      playButton.setAttribute('class', 'play');
+      div.appendChild(playButton);
+      div.onclick = function () {
+        labnolIframe(this);
+      };
+      playerElements[n].appendChild(div);
+    }
+  }
+
+  document.addEventListener('DOMContentLoaded', initYouTubeVideos);
