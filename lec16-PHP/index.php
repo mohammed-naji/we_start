15fd20be965a9980004e3d4fd1a2cd43231ea472
Mohammed Naji <m.naji1512@gmail.com>
2022-10-24 09:54:19 +0300

    lec16

---
+++ lec16-PHP/index.php
@@ -0,0 +1,82 @@
+<?php 
+$name = "Mohammed";
+$age = 28;
+
+// $name = 'Ali';
+
+// function fact($x) {
+//     // $res = 1;
+//     // for($i = 1 ; $i <= $x ; $i++) {
+//     //     $res = $res * $i;
+//     // }
+
+//     // echo $res;
+
+//     // return $x * fact($x - 1);
+
+//     if($x <= 1) {
+//         return 1;
+//     }else {
+//         return $x * fact($x - 1);
+//     }
+// }
+
+// echo fact(4);
+
+// echo 'Welcome ' . $name . ', your age is '. $age;
+
+// $sql = "INSERT INTO users (name, age) VALUES ('".$name."', 28)";
+// $sql = "INSERT INTO users (name, age) VALUES ('$name', '$age')";
+
+// echo "W";
+// echo "e";
+// echo "l";
+// echo "c";
+// echo "o";
+// echo "m";
+// echo "e";
+// echo " ";
+// // echo ("ffff");
+// // print ("ffff");
+// echo $name;
+// echo " ";
+// echo "your age is";
+// echo " ";
+// echo $age;
+
+
+// if(Condtion) {
+//     Action
+// }
+
+// if(Condtion) {
+//     Action
+// }else {
+    // Another Action
+// }
+
+// if(Condtion) {
+//     Action
+// elsif () {
+//     Else Action
+// }
+// }else {
+    // Another Action
+// }
+
+// switch() {
+//     case '' :
+
+//     break;
+
+//     case '' :
+
+//     break;
+//     default:
+
+// }
+
+// Array 
+// 1. Index Array 
+// 2. Multi Dimension 
+// 3. Assoicatice Array
